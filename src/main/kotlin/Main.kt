2e48571c746f295918fbef4a57f3a6f8342666cd
kesa-edna-adhiambo <ednakesa@gmail.com>
2024-02-07 07:30:29 +0300

    added one file

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,17 @@
+fun main(args: Array<String>) {
+  var name = "Edna Kesa"
+    println(name)
+
+    var age = "21"
+  println(age)
+
+    var phonenumber = "0711242173"
+  println(phonenumber)
+
+    var weight = "62"
+  println(weight)
+
+    var citizen = "kenyan"
+  println(citizen)
+
+}
